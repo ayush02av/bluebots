20de5647da0bfbf34edc3cd401446aaee6d60f16
Ayush Verma <ayush02av@gmail.com>
2021-09-04 12:00:08 +0530

    added icon

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,10 @@
 $("#head-container").load("/components/header.html");
 $("#foot-container").load("/components/footer.html");
+
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = '/favicon.ico';
