e88333553aef8465dffe20bf781074e8f404d65d
Ayush Verma <ayush02av@gmail.com>
2021-09-05 13:47:10 +0530

    updated and fixed errors

---
+++ scripts/topics/reactroadmap.js
@@ -0,0 +1,54 @@
+const nodes_container = document.getElementById('roadmap-nodes-container');
+
+const roadmap = [
+    {
+        heading:'HTML 5',
+        content:'Learn Hyper Text Markup Language that sits at the base of every webpage. Bone structure of every webpage.',
+        link:'https://www.youtube.com/playlist?list=PLr6-GrHUlVf_ZNmuQSXdS197Oyr1L9sPB'
+    },
+    {
+        heading:'CSS 3',
+        content:'Learn Cascading Style Sheets, a language used to design webpages, & make them attractive.',
+        link:'https://youtu.be/Edsxf_NBFrw'
+    },
+    {
+        heading:'Javascript es6',
+        content:'The most interesting aspect - Javascript. Cool animations, intuitive and interactive webpages, you name it.',
+        link:'https://youtu.be/nZ1DMMsyVyI'
+    },
+    {
+        heading:'DOM manipulation',
+        content:'Create & manipulate HTML elements using JS...interesting, isn\'t it?',
+        link:'https://www.youtube.com/playlist?list=PLsyeobzWxl7qtP8Lo9TReqUMkiOp446cV'
+    },
+    {
+        heading:'Bootstrap',
+        content:'Beautiful CSS classes, ready to use, thats Bootstrap for you',
+        link:'https://youtu.be/vpAJ0s5S2t0'
+    },
+    {
+        heading:'jQuery',
+        content:'Beginner friendly javascript framework for interactive webpages',
+        link:'https://youtu.be/HgvIox6ehkM'
+    },
+    {
+        heading:'JS Framework',
+        content:'Learn some extensive javascript framework like React or Angular',
+        link:'https://youtu.be/RGKi6LSPDLU'
+    },
+    {
+        heading:'Bash Command Line',
+        content:'Show off "programming skills" in front of your non-tech friends by using this cool tool',
+        link:'https://youtu.be/oxuRxtrO2Ag'
+    },
+    {
+        heading:'Git & GitHub',
+        content:'Perfect version control utility software you\'ll ever need',
+        link:'https://youtu.be/USjZcfj8yxE'
+    },
+    {
+        heading:'Projects',
+        content:'Practice leads to Excellence. Test your skills by working on various projects',
+        link:'https://youtu.be/0IOpVkIabsI'
+    }
+]
