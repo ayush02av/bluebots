326b8e4e1034a8c6e789dc73238313d97c4880e7
Ayush Verma <ayush02av@gmail.com>
2021-09-05 15:31:25 +0530

    added react roadmap

--- scripts/topics/reactroadmap.js
+++ scripts/topics/reactroadmap.js
@@ -2,53 +2,53 @@ const nodes_container = document.getElementById('roadmap-nodes-container');
 
 const roadmap = [
     {
-        heading:'HTML 5',
-        content:'Learn Hyper Text Markup Language that sits at the base of every webpage. Bone structure of every webpage.',
-        link:'https://www.youtube.com/playlist?list=PLr6-GrHUlVf_ZNmuQSXdS197Oyr1L9sPB'
+        heading:'HTML & CSS basics',
+        content:'Cover HTML & CSS basics for webpage structure and design.',
+        link:'https://youtu.be/QMnv3QrjZoU'
     },
     {
-        heading:'CSS 3',
-        content:'Learn Cascading Style Sheets, a language used to design webpages, & make them attractive.',
-        link:'https://youtu.be/Edsxf_NBFrw'
+        heading:'Javascript Basics',
+        content:'Cover & practice all Javascript basics- variables, loops, functions etc.',
+        link:'https://youtu.be/W6NZfCO5SIk'
     },
     {
-        heading:'Javascript es6',
-        content:'The most interesting aspect - Javascript. Cool animations, intuitive and interactive webpages, you name it.',
-        link:'https://youtu.be/nZ1DMMsyVyI'
+        heading:'Node',
+        content:'Setup Node JS - a runtime environment for locally running Javascript programs',
+        link:'https://youtu.be/TlB_eWDSMt4'
     },
     {
-        heading:'DOM manipulation',
-        content:'Create & manipulate HTML elements using JS...interesting, isn\'t it?',
-        link:'https://www.youtube.com/playlist?list=PLsyeobzWxl7qtP8Lo9TReqUMkiOp446cV'
+        heading:'NPM & NPX',
+        content:'Setup NPM - node package manager to install various libraries like React itself',
+        link:'https://youtu.be/2V1UUhBJ62Y'
     },
     {
-        heading:'Bootstrap',
-        content:'Beautiful CSS classes, ready to use, thats Bootstrap for you',
-        link:'https://youtu.be/vpAJ0s5S2t0'
-    },
-    {
-        heading:'jQuery',
-        content:'Beginner friendly javascript framework for interactive webpages',
-        link:'https://youtu.be/HgvIox6ehkM'
-    },
-    {
-        heading:'JS Framework',
-        content:'Learn some extensive javascript framework like React or Angular',
+        heading:'React Setup',
+        content:'Setup first React Project & start running it in development environment',
         link:'https://youtu.be/RGKi6LSPDLU'
     },
     {
-        heading:'Bash Command Line',
-        content:'Show off "programming skills" in front of your non-tech friends by using this cool tool',
-        link:'https://youtu.be/oxuRxtrO2Ag'
+        heading:'First Project',
+        content:'Make a small portfolio or to-do project',
+        link:'https://youtu.be/pCA4qpQDZD8'
     },
     {
-        heading:'Git & GitHub',
-        content:'Perfect version control utility software you\'ll ever need',
+        heading:'Project on GitHub',
+        content:'Push your project on GitHub',
         link:'https://youtu.be/USjZcfj8yxE'
     },
     {
-        heading:'Projects',
-        content:'Practice leads to Excellence. Test your skills by working on various projects',
-        link:'https://youtu.be/0IOpVkIabsI'
+        heading:'Deploy on Netlify',
+        content:'Deploy your portfolio project on Netlify & share with peers',
+        link:'https://youtu.be/8KGLOJHqNMg'
+    },
+    {
+        heading:'Integrate Backend',
+        content:'Choose your preferred backend tech, to add more features in your projects',
+        link:'https://www.youtube.com/playlist?list=PLzMcBGfZo4-kCLWnGmK0jUBmGLaJxvi4j'
+    },
+    {
+        heading:'Scale Projects',
+        content:'Scale up or develop more projects & enhance your skills',
+        link:'https://youtu.be/a_7Z7C_JCyo'
     }
 ]
